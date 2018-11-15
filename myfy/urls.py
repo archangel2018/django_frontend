07956904c085e73cdc2528fe18108f9ea0bd0616
arun.m <arun_m91@outlook.com>
2018-11-15 20:01:28 +0530

    added json payload + regex + ^

--- myfy/urls.py
+++ myfy/urls.py
@@ -20,6 +20,6 @@ from pyfy import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.HomeView.as_view()),
-    path(r'ping/?', views.ping),
-    path(r'msg/?', views.msg),
+    path(r'^ping/?', views.ping),
+    path(r'^msg/?', views.msg),
 ]
