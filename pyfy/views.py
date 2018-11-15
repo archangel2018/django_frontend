15283a0a16a051166d57c8753d885f39f931e80d
arun.m <arun_m91@outlook.com>
2018-11-15 19:59:07 +0530

    added json payload

--- pyfy/views.py
+++ pyfy/views.py
@@ -2,10 +2,19 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.http import HttpResponse
+import requests
 
 class HomeView(TemplateView):
     template_name = 'index.html'
 
+import ipdb
+
 
 def ping(request):
     return HttpResponse('pong')
+
+def msg(request):
+    ipdb.set_trace()
+    url = 'http: // backend2.geekyarun.com:8080 / demo / msg'
+    r = requests.get('url')
+    return HttpResponse(r.json())
